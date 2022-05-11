047d1df14178e0adf241a615b9f3d70fded849b6
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-11 20:30:39 +0600

    Add files via upload

---
+++ Difference/Difference.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int A,B,C,D,Total;
+    cin>>A>>B>>C>>D;
+    Total = (A*B)-(C*D);
+    cout<<"DIFERENCA = "<<Total<<endl;
+    return 0;
+
+}
